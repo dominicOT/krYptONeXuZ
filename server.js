701fa673c6a9a8dc483894f8b38aaa70acd123d5
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-10 17:29:57 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const path = require('path');
+
+// Array to store messages
+const messages = [];
+
+io.on('connection', (socket) => {
+  console.log('A user connected - online');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected - offline');
+  });
+
+  socket.on('chat message', (fullMessage) => {
+    console.log('Received message:', fullMessage);
+
+  const messageParts = fullMessage.split(':');
+  if (messageParts.length !== 2) {
+    console.log('Invalid message format:', fullMessage);
+    return;
+  }
+
+  const username = messageParts[0].trim();
+  const message = messageParts[1].trim();
+
+    const formattedMessage = {
+      username: username,
+      content: message,
+    };
+    const newMessage = formattedMessage;
+//    const newMessage = { username, message };
+
+    // Store the message in the array
+    messages.push(newMessage);
+
+
+    io.emit('chat message', newMessage);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
