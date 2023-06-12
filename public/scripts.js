0abadfc6e6da1349aa5c89b648b4f5d0ded285b4
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-12 20:37:31 +0100

    Create scripts.js

---
+++ public/scripts.js
@@ -0,0 +1,24 @@
+const socket = io();
+
+const form = document.getElementById('message-form');
+const usernameInput = document.getElementById('input-username');
+const input = document.getElementById('input-message');
+const messages = document.getElementById('messages');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const username = usernameInput.value.trim();
+  const message = input.value.trim();
+
+  if (username !== '' && message !== '') {
+    const fullMessage = `${username}: ${message}`;
+    socket.emit('chat message', fullMessage);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', (message) => {
+const li = document.createElement('li');
+li.textContent = `${message.username}: ${message.content}`;
+messages.appendChild(li);
+});
