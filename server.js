c15cadcb75ecb494f6e3f1ed5475324016fce2ca
Dominic Oladapo-Tonade <110516773+dominicOT@users.noreply.github.com>
2023-06-12 20:51:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ io.on('connection', (socket) => {
   });
 });
 
+// Serve static files
+app.use(express.static('public'));
+
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
